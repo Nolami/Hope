21212c52a3a3fa72459021b47f343b8265a6fdd7
Nolami <deata7660@gmail.com>
2024-01-16 13:41:10 +0200

    we did

--- code.cs
+++ code.cs
@@ -89,3 +89,49 @@ namespace Youch
             }
     }
 }
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Beep // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wil = "Tehtävä 11 // Egert Sulger";
+            Console.WriteLine(wil);
+            Console.WriteLine();
+            
+            for (int i = 0; i < wil.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            int vi = 0; // counter
+            int val = 0; //value itself, doesn't need to be a global but too late now
+            List<int> ni = new List<int>(); //Actual list
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write("{0}. listan luku ? ", vi +1); //Question
+                val = int.Parse(Console.ReadLine()); //value
+                Console.WriteLine();
+                ni.Add(val); //Add value to list
+                vi++;
+
+            }
+            Console.WriteLine();
+            foreach (int von in ni) // take all values from the list
+            {
+                Console.WriteLine(von); //Print those values
+            }
+
+        }   
+
+
+    }
+
+ }
